83110fe7aca9b465d328c4bf0591adc5c3eaddf4
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2022-11-01 18:00:04 -0300

    UART 2 É ASSIM Q MANDA E RECEBE OK

---
+++ uart2.c
@@ -0,0 +1,49 @@
+//gcc test.c -o test.run -lwiringPi
+
+//sudo /test.run
+
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+#define BAUD_RATE 9600
+
+int main (void) {
+        int serial_port;
+        char dat;
+
+        if ((serial_port = serialOpen ("/dev/ttyS0", BAUD_RATE)) < 0) {
+                fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+                return 1 ;
+        }
+
+        if (wiringPiSetup () == -1)
+        {
+                fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+                return 1;
+        }
+        
+        printf("Port: %d \n", serial_port);
+        dat = serialGetchar (serial_port);
+        printf ("Dat: %d \n", dat);
+        fflush (stdout) ;
+        serialPutchar(serial_port, 0x00);
+        dat = serialGetchar (serial_port);
+        printf ("Dat: %d \n", dat) ;
+        
+        /*while(1){
+                if(serialDataAvail (serial_port) ) {
+                        printf("Port: %c \n", serial_port);
+                        dat = serialGetchar (serial_port);
+                        printf ("Dat: %c \n", dat) ;
+                        fflush (stdout) ;
+                        serialPutchar(serial_port, dat);
+                }
+        } */
+        
+        return 0;
+}
