32a8bbd84f24c98c4af84a5acb21725cbee48a39
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2022-11-01 18:22:25 -0300

    UART 2 MELHOR

--- uart2.c
+++ uart2.c
@@ -14,7 +14,7 @@
 
 int main (void) {
         int serial_port;
-        char dat;
+        char dat, output_address, output_command, input_address, input_command;
 
         if ((serial_port = serialOpen ("/dev/ttyS0", BAUD_RATE)) < 0) {
                 fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
@@ -35,15 +35,38 @@ int main (void) {
         dat = serialGetchar (serial_port);
         printf ("Dat: %d \n", dat) ;
         
-        /*while(1){
-                if(serialDataAvail (serial_port) ) {
-                        printf("Port: %c \n", serial_port);
-                        dat = serialGetchar (serial_port);
-                        printf ("Dat: %c \n", dat) ;
-                        fflush (stdout) ;
-                        serialPutchar(serial_port, dat);
-                }
-        } */
+        output_command = 0x03;
+        output_address = 0x03;
+        
+        //if(serialDataAvail (serial_port) > 0){
+            serialPutchar(serial_port, output_command);
+            serialPutchar(serial_port, output_address);
+        
+            input_command = serialGetchar(serial_port);
+            input_address = serialGetchar(serial_port);
+        
+            printf("out: %d , %d \n", output_address, output_command, output_address, output_command);
+            printf("in: %d , %d, %X , %X \n", input_address, input_command, input_address, input_command);
+        
+            if(input_command == 0x00  && input_address == 0x00){
+               printf("NodeMCU funcionando normalmente!");
+               //return 0x00;
+            }
+        
+            else if(input_command == 0x1F  && input_address == 0x1F){
+               printf("NodeMCU com problema!");
+               //return 0x1F;
+            }
+        
+            else{
+                printf("Problema desconhecido detectado!");
+                //return 0x1F;
+            }
+        //}
+        //else{
+           //printf("Problema desconhecido detectado!!  %d \n \n",serial_port);
+           //return 0x1F;
+       //}
         
         return 0;
 }
