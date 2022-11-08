ad7fd96608fb2786da9f625a7dac8ee4a2a49a94
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2022-11-08 18:07:19 -0300

    enviando e quase recebendo

--- uart2.c
+++ uart2.c
@@ -29,9 +29,9 @@
 
 #define BAUD_RATE 9600
 
-void sendData(int port, char addr, char comm);
+void sendData(int port, char *comm);
 void sendDataDigitalInput(int port, char addr, int comm);
-void recData(int port, char *addr, char *comm);
+void recData(int port, char *comm, char *addr);
 void recDataInput(int port, char *addr, char *comm, char *val);
 void evaluateRecData(char addr, char comm, int value);
 
@@ -39,21 +39,21 @@ void evaluateRecData(char addr, char comm, int value);
 int main (void) {
         setlocale(LC_ALL,"Portuguese");			// TO ACTIVATE PORTUGUESE CHARACTERS
 
-        int serial_port, i = 0, digital_input_comm = 50;
-        unsigned char output_address, output_command, input_address, input_command, input_value;
+        int serial_port, i = 0, digital_input_comm = 49;
+        char *output_command, *input_command, *input_address, *input_value;
 
         if ((serial_port = serialOpen ("/dev/ttyS0", BAUD_RATE)) < 0) {
-                fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-                return 1 ;
+                fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno));
+                return 1;
         }
 
         if (wiringPiSetup () == -1)
         {
-                fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+                fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno));
                 return 1;
         }
-
-        system("cls || clear");
+        
+        //system("cls || clear");
         printf("PBL - Interfaces de E/S. \n \n");
         printf("As ações seguirão a seguinte ordem: \n");
         printf("1 - Solicita a situa atual do NodeMCU. \n");
@@ -62,89 +62,108 @@ int main (void) {
         printf("4 - Acendimento do led da NodeMCU. \n");
         printf("5 - Desligamento do led da NodeMCU. \n \n");
         sleep(4);
+        
+        printf("Ação: Solicita a situação atual do NodeMCU. \n \n");
+        sleep(1);
 
-        for(i = 0; i <= 4; i++){
+       output_command = "30";
+       
+       fflush (stdout);
+       serialFlush(serial_port);
+       sendData(serial_port, "30");
+
+       printf("comm out: %s \n ", output_command);
+
+       recData(serial_port, input_command, input_address);
+       //printf("comm in 0: %s \n \n", input_command);
+
+        /*for(i = 0; i <= 4; i++){
             if(i == 0){                   //write 0x03 twice; this checks the NodeMCU status.
                printf("Ação: Solicita a situação atual do NodeMCU. \n \n");
-               sleep(4);
+               sleep(1);
 
-               output_command = 0x03;
-               output_address = 0x03;
-               sendData(serial_port, output_address, output_command);
+               output_command = "33";
+               //output_address = '3';
+               
+               fflush (stdout);
+               serialFlush(serial_port);
+               sendData(serial_port, output_command);
 
-               printf("addr out: %X , %d \n ", output_address, output_address);
-               printf("comm out: %X , %d \n ", output_command, output_command);
-               printf("addr in 0: %X , %d \n ", input_address, input_address);
-               printf("comm in 0: %X , %d \n ", input_command, input_command);
+               printf("comm out: %X , %d , %s \n ", output_command, output_command, output_command);
+               printf("addr in 0: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 0: %X , %d , %c \n ", input_command, input_command, input_command);
 
                recData(serial_port, &input_address, &input_command);
-               printf("addr in 0: %X , %d \n ", input_address, input_address);
-               printf("comm in 0: %X , %d \n \n", input_command, input_command);
-               evaluateRecData(input_address, input_command, 0);
+               printf("addr in 0: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 0: %X , %d , %c \n \n", input_command, input_command, input_command);
+               //evaluateRecData(input_address, input_command, 0);
             }
 
             if(i == 1){            //write 0x04 twice; this requests analog input value
                //system("cls || clear");
                printf("Ação: Solicita o valor da entrada analógica. \n \n");
-               sleep(4);
+               sleep(1);
 
-               output_command = 0x04;
-               output_address = 0x04;
-               sendData(serial_port, output_address, output_command);
+               output_command = "40";
+               
+               fflush (stdout);
+               serialFlush(serial_port);
+               sendData(serial_port, output_command);
 
-               printf("addr out: %X , %d \n ", output_address, output_address);
-               printf("comm out: %X , %d \n ", output_command, output_command);
-               printf("addr in 1: %X , %d \n ", input_address, input_address);
-               printf("comm in 1: %X , %d \n ", input_command, input_command);
+               printf("comm out: %X , %d , %s \n ", output_command, output_command, output_command);
+               printf("addr in 1: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 1: %X , %d , %c \n ", input_command, input_command, input_command);
 
                recDataInput(serial_port, &input_address, &input_command, &input_value);
-               printf("addr in 1: %X , %d \n ", input_address, input_address);
-               printf("comm in 1: %X , %d \n \n", input_command, input_command);
-               evaluateRecData(input_address, input_command, input_value);
+               printf("addr in 1: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 1: %X , %d , %c \n \n", input_command, input_command, input_command);
+               //evaluateRecData(input_address, input_command, input_value);
             }
 
             if(i == 2){            //this requests some digital input value
                //system("cls || clear");
                printf("Ação: Solicita o valor de uma das entradas digitais. \n \n");
-               sleep(4);
+               sleep(1);
 
-               output_command = 0x05;
-               output_address = 0x05;   // IMPLEMENT HERE HOW TO CHOOSE WHAT INPUT WILL BE READ
+               output_command = '5';
 
-               for (digital_input_comm = 50; digital_input_comm <= 52; digital_input_comm++){   //LOOP TO COMMUTE DIGITAL SENSORS
+               for (digital_input_comm = 49; digital_input_comm <= 58; digital_input_comm++){   //LOOP TO COMMUTE DIGITAL SENSORS
                    printf("Entrada digital: %d. \n \n", digital_input_comm);
-                   sendDataDigitalInput(serial_port, output_address, digital_input_comm);
+                   
+                   fflush (stdout);
+                   serialFlush(serial_port);
+                   sendDataDigitalInput(serial_port, output_command);
 
-                   printf("addr out: %X , %d \n ", output_address, output_address);
-                   printf("comm out: %X , %d \n ", digital_input_comm, digital_input_comm);
-                   printf("addr in 2: %X , %d \n ", input_address, input_address);
-                   printf("comm in 2: %X , %d \n ", input_command, input_command);
+                   printf("comm out: %X , %d , %s \n ", output_command, output_command, output_command);
+                   printf("addr in 2: %X , %d , %c \n ", input_address, input_address, input_address);
+                   printf("comm in 2: %X , %d , %c \n ", input_command, input_command, input_command);
 
                    recDataInput(serial_port, &input_address, &input_command, &input_value);
-                   printf("addr in 2: %X , %d \n ", input_address, input_address);
-                   printf("comm in 2: %X , %d \n \n", input_command, input_command);
-                   printf("val in 2: %X , %d \n \n", input_value, input_value);
-                   evaluateRecData(input_address, input_command, input_value);
+                   printf("addr in 1: %X , %d , %c \n ", input_address, input_address, input_address);
+                   printf("comm in 1: %X , %d , %c \n ", input_command, input_command, input_command);
+                   printf("val in 2: %X , %d , %c \n \n", input_value, input_value, input_value);
+                   //evaluateRecData(input_address, input_command, input_value);
                }
             }
 
             if(i == 3){            //this turn on the led
                //system("cls || clear");
                printf("Ação: Acendimento do led da NodeMCU. \n \n");
-               sleep(4);
+               sleep(1);
 
-               output_command = 0x06;
-               output_address = 0x04;   // THIS VALUE MUST BE THE BUILTIN LED PIN
-               sendData(serial_port, output_address, output_command);
+               output_command = "60";  // THIS VALUE MUST BE THE BUILTIN LED PIN
+               
+               fflush (stdout);
+               serialFlush(serial_port);
+               sendData(serial_port, output_command);
 
-               printf("addr out: %X , %d \n ", output_address, output_address);
-               printf("comm out: %X , %d \n ", output_command, output_command);
-               printf("addr in 3: %X , %d \n ", input_address, input_address);
-               printf("comm in 3: %X , %d \n ", input_command, input_command);
+               printf("comm out: %X , %d , %s \n ", output_command, output_command, output_command);
+               printf("addr in 3: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 3: %X , %d , %c \n ", input_command, input_command, input_command);
 
                recData(serial_port, &input_address, &input_command);
-               printf("addr in 3: %X , %d \n ", input_address, input_address);
-               printf("comm in 3: %X , %d \n \n", input_command, input_command);
+               printf("addr in 3: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 3: %X , %d , %c \n ", input_command, input_command, input_command);
             }
 
             if(i == 4){            //this turn off the led
@@ -152,45 +171,55 @@ int main (void) {
 
                //system("cls || clear");
                printf("Ação: Desligamento do led da NodeMCU. \n \n");
-               sleep(4);
+               sleep(1);
 
-               output_command = 0x07;
-               output_address = 0x04;   // THIS VALUE MUST BE THE BUILTIN LED PIN
-               sendData(serial_port, output_address, output_command);
+               output_command = "70";   // THIS VALUE MUST BE THE BUILTIN LED PIN
+               
+               fflush (stdout);
+               serialFlush(serial_port);
+               sendData(serial_port, output_command);
 
-               printf("addr out: %X , %d \n ", output_address, output_address);       
-               printf("comm out: %X , %d \n ", output_command, output_command);   
-               printf("addr in 4: %X , %d \n ", input_address, input_address); 
-               printf("comm in 4: %X , %d \n ", input_command, input_command); 
+               printf("comm out: %X , %d , %s \n ", output_command, output_command, output_command);
+               printf("addr in 4: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 4: %X , %d , %c \n ", input_command, input_command, input_command);
 
                recData(serial_port, &input_address, &input_command);
-               printf("addr in 4: %X , %d \n ", input_address, input_address);
-               printf("comm in 4: %X , %d \n \n", input_command, input_command);
+               printf("addr in 4: %X , %d , %c \n ", input_address, input_address, input_address);
+               printf("comm in 4: %X , %d , %c \n ", input_command, input_command, input_command);
             }
 
-        }
+        } */
 
         return 0;
 }
 
-void sendData(int port, char addr, char comm){
-     serialPutchar(port, addr);
-     serialPutchar(port, comm);
+void sendData(int port, char *comm){
+     serialPuts(port, comm);
 }
 
 void sendDataDigitalInput(int port, char addr, int comm){
-     serialPutchar(port, addr);
      serialPutchar(port, comm);
+     serialPutchar(port, addr);
 }
 
-void recData(int port, char *addr, char *comm){
-     *addr = serialGetchar(port);
-     *comm = serialGetchar(port);
+void recData(int port, char *comm, char *addr){
+    // memset(comm, 0, strlen(comm));
+     //memset(comm, 0, strlen(addr));
+     
+     char buffer[2];
+     
+     //sleep(2);
+     buffer[0] = serialGetchar(port);
+     printf("GETCHAR 0: %s \n \n", buffer);
+     buffer[1] = serialGetchar(port);
+     printf("GETCHAR 1: %s \n \n", buffer);
+     comm = buffer;
+     //strcat(comm, addr);
 }
 
 void recDataInput(int port, char *addr, char *comm, char *val){
-     *addr = serialGetchar(port);
      *comm = serialGetchar(port);
+     *addr = serialGetchar(port);
      *val  = serialGetchar(port);
 }
 
@@ -217,8 +246,8 @@ void evaluateRecData(char addr, char comm, int value){
     }
 
     else{
-        printf("Resposta desconhecida: %X. \n", comm);
-        printf("Valor recebido: %d. \n \n", value);
+        printf("Resposta desconhecida: %X , %c. \n", comm, comm);
+        printf("Valor recebido: %d , %c. \n \n", value);
        // escreve no display
     }
 }
